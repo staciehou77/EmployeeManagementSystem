4be795e8a1ffa6a52bdfa0298dfba81524ad2fec
Stacie Hou <you@example.comkaitiy2001@gmail.com>
2022-03-10 17:02:14 +0800

    add CRUD function

---
+++ EmployeeManagementSystem/Controllers/EMPLOYEEsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeManagementSystem.Models;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class EMPLOYEEsController : Controller
+    {
+        private EmployeeEntities db = new EmployeeEntities();
+
+        // GET: EMPLOYEEs
+        public ActionResult Index()
+        {
+            return View(db.EMPLOYEE.ToList());
+        }
+
+        // GET: EMPLOYEEs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEE eMPLOYEE = db.EMPLOYEE.Find(id);
+            if (eMPLOYEE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEE);
+        }
+
+        // GET: EMPLOYEEs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EMPLOYEEs/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Emp_NO,Emp_Name,Emp_Location,Emp_Phone,Dept_No,Dept_Name,Salary,Job_ID")] EMPLOYEE eMPLOYEE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EMPLOYEE.Add(eMPLOYEE);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eMPLOYEE);
+        }
+
+        // GET: EMPLOYEEs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEE eMPLOYEE = db.EMPLOYEE.Find(id);
+            if (eMPLOYEE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEE);
+        }
+
+        // POST: EMPLOYEEs/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Emp_NO,Emp_Name,Emp_Location,Emp_Phone,Dept_No,Dept_Name,Salary,Job_ID")] EMPLOYEE eMPLOYEE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eMPLOYEE).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eMPLOYEE);
+        }
+
+        // GET: EMPLOYEEs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEE eMPLOYEE = db.EMPLOYEE.Find(id);
+            if (eMPLOYEE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEE);
+        }
+
+        // POST: EMPLOYEEs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EMPLOYEE eMPLOYEE = db.EMPLOYEE.Find(id);
+            db.EMPLOYEE.Remove(eMPLOYEE);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
